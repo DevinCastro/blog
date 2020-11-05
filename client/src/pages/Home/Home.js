f41bea2b6269dcf24ddf3cf3b1a4795a1fd06143
DevinCastro <dcrocker21@gmail.com>
2020-11-05 00:28:11 -0800

    blog is set up and ready to link to backend

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Jumbotron, Button, Form, FormGroup, Label, Input, } from 'reactstrap';
 
 const Home = () => {
+
+
+  const [blogState, setBlogState] = useState({
+    blog: "",
+    blogs: []
+  })
+
+  blogState.handleInputChange = event => {
+    setBlogState({ ...blogState, [event.target.name]: event.target.value })
+  }
+
+
+  blogState.handleAddBlog = event => {
+    event.preventDefault()
+
+    console.log(blogState)
+
+
+    
+  }
+
   return (
-    <h1>This is the home page</h1>
+    <>
+    <Container>
+
+      <Jumbotron>
+        <h1>Write a Blog</h1>
+        
+        <Form>
+          <FormGroup>
+            <Input
+            type="textarea"
+            name="blog"
+            value={blogState.blog}
+            onChange={blogState.handleInputChange}
+            />
+            <br />
+    
+            <Button onClick={blogState.handleAddBlog}>Add Blog</Button>
+
+          </FormGroup>
+        </Form>
+
+      </Jumbotron>
+    </Container>
+    </>
   )
 }
 
